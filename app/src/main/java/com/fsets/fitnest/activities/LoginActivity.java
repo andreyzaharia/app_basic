55326a021f909653e88c6076cc3d1c06ce6744cf
Fsets <andreyzaharia41@gmail.com>
2024-02-29 15:13:32 +0100

    commit

---
+++ app/src/main/java/com/fsets/fitnest/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.fsets.fitnest.activities;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fsets.fitnest.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_);
+    }
+}
